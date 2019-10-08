f551d2e433c3203621e82343d252fd6711ea9d8a
apierc2175 <aidanwpierce@gmail.com>
2019-10-08 14:45:02 -0400

    added true false

--- icecream/models.py
+++ icecream/models.py
@@ -1,14 +1,31 @@
 from django.db import models
 from django.utils import timezone
 # Create your models here.
+CHO = 'CHOCOLATE'
+VAN = 'VANILLA'
+
+DAI = 'DAILY'
+WEE = 'WEEKLY'
+SEA = 'SEASONAL'
+
 
 class IceCream(models.Model):
     flavor_text = models.CharField(max_length=30)
-    date_churned = models.DateTimeField('date published', default=timezone.now)
+    base = [
+        (CHO, 'Chocolate'),
+        (VAN, 'Vanilla'),
+    ]
+    available = [
+        (DAI, 'Daily'),
+        (WEE, 'Weekly'),
+        (SEA, 'Seasonal'),
+    ]
+    featured = models.BooleanField(default=False)
+    date_churned = models.DateTimeField('date churned', default=timezone.now)
 
     def __str__(self):
         return self.flavor_text
-# 
+#
 # class Choice(models.Model):
 #      flavor = models.ForeignKey(IceCream, on_delete=models.CASCADE)
 #      choice_text = models.CharField(max_length=30)
