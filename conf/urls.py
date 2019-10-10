49dbe9dad7062918c62243719b196fd83dcf2b20
apierc2175 <aidanwpierce@gmail.com>
2019-10-10 12:04:16 -0400

    automated url

--- conf/urls.py
+++ conf/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('icecream/', include('icecream.urls', namespace='icecream')),
+    path('', include('icecream.urls', namespace='icecream')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts:logout/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
